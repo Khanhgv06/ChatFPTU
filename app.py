066254dca307b96387c0dc16bdb65f55974653af
username <emanhmc@gmail.com>
2025-03-08 11:12:49 +0700

    Change to port 443

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def send_to_messenger(recipient_id, response_text):
         print("Error sending to Messenger:", response.text)
 
 if __name__ == "__main__":
-    app.run(port=4869, debug=True)
+    app.run(port=443, debug=True)
